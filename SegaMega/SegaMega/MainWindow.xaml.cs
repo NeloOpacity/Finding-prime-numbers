ec281d200fe6f51237e5015869cbc9080c91186e
NeloOpacity <volleycouch@gmail.com>
2019-01-19 17:33:33 +0300

    E

---
+++ SegaMega/SegaMega/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SegaMega
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker backgroundWorker;
+        public MainWindow()
+        {
+            InitializeComponent();
+            backgroundWorker = (BackgroundWorker)this.FindResource("backgroundWorker");
+        }
+
+        private void bSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            bSubmit.IsEnabled = false;
+            bCancel.IsEnabled = true;
+            Result.Items.Clear();
+            int from, to;
+            try
+            {
+                from = Int32.Parse(From.Text.Trim());
+                to = Int32.Parse(To.Text.Trim());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("В полях для ввода некорректные значения");
+                return;
+            }
+            PrimesInput input = new PrimesInput(from, to);
+            backgroundWorker.RunWorkerAsync(input);
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            PrimesInput input = (PrimesInput)e.Argument;
+            int[] primes = Worker.FindPrimes(input,backgroundWorker);
+            if (backgroundWorker.CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+            e.Result = primes;
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                MessageBox.Show("Поиск отменён");
+            }
+            else
+            {
+                int[] primes = (int[])e.Result;
+                foreach(var p in primes)
+                {
+                    Result.Items.Add(p);
+                }
+            }
+            bSubmit.IsEnabled = true;
+            bCancel.IsEnabled = false;
+            progressBar.Value = 0;
+        }
+
+        private void bCancel_Click(object sender, RoutedEventArgs e)
+        {
+            backgroundWorker.CancelAsync();
+            bSubmit.IsEnabled = true;
+            bCancel.IsEnabled = false;
+        }
+    }
+}
---
+++ SegaMega/SegaMega/PrimesInput.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegaMega
+{
+    class PrimesInput
+    {
+        public int from, to;
+        public PrimesInput(int _from, int _to)
+        {
+            from = _from;
+            to = _to;
+        }
+    }
+    static class Worker
+    {
+        static public int[] FindPrimes(PrimesInput input)
+        {
+            return FindPrimes(input, null);
+        }
+
+        static public int[] FindPrimes(PrimesInput input, BackgroundWorker backgroundWorker)
+        {
+            int num = input.from;
+            int[] arr = new int[input.to - input.from];
+            int iteration = arr.Length / 100;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = num;
+                num++;
+            }
+            int maxDiv = (int)Math.Floor(Math.Sqrt(input.to));
+            int[] marks = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 2; j <= maxDiv; j++)
+                {
+                    if ((arr[i] != j) && (arr[i] % j == 0))
+                    {
+                        marks[i] = 1;
+                    }
+                }
+                if ((i % iteration == 0) && (backgroundWorker != null))
+                {
+                    if (backgroundWorker.CancellationPending)
+                    {
+                        return null;
+                    }
+                    if (backgroundWorker.WorkerReportsProgress)
+                    {
+                        backgroundWorker.ReportProgress(i / iteration);
+                    }
+                }
+            }
+            int primes = 0;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i] == 0)
+                    primes++;
+            }
+            int[] ret = new int[primes];
+            int curs = 0;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i] == 0)
+                {
+                    ret[curs] = arr[i];
+                    curs++;
+                }
+            }
+            if (backgroundWorker != null && backgroundWorker.WorkerReportsProgress)
+            {
+                backgroundWorker.ReportProgress(100);
+            }
+            return ret;
+        }
+    }
+}
